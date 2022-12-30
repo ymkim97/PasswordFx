a42eefe07dbd91ec04de2bbb05da711b44b363f2
ymkim97 <rladudaud97@naver.com>
2022-12-30 14:36:48 +0900

    feat: login main GUI and logout button

--- src/main/java/ymkim/passwordfx/LoggedController.java
+++ src/main/java/ymkim/passwordfx/LoggedController.java
@@ -1,4 +1,28 @@
 package ymkim.passwordfx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class LoggedController {
+
+    @FXML
+    private AnchorPane loggedPane;
+
+    public void setLogoutButton() {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want to logout?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            Stage stage = (Stage) loggedPane.getScene().getWindow();
+            stage.close();
+        }
+
+    }
+
 }
