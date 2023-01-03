16b6e981f513870b625d52a2bd6b658b4d8ea54e
ymkim97 <rladudaud97@naver.com>
2023-01-03 16:01:24 +0900

    feat: main user registration using database

--- src/main/java/ymkim/passwordfx/RegisterController.java
+++ src/main/java/ymkim/passwordfx/RegisterController.java
@@ -1,16 +1,19 @@
 package ymkim.passwordfx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class RegisterController {
 
     H2Connector h2Connector = new H2Connector();
+    boolean isDuplicate = true;
 
     @FXML
     AnchorPane registerPane;
@@ -30,25 +33,95 @@ public class RegisterController {
     }
 
     public void setSignupButton() {
-        try {
-            Connection con = h2Connector.getConnection();
-            Statement stmt = con.createStatement();
-            String state = "INSERT INTO MAINUSER VALUES('" + usernameInput.getText() + "','" + nameInput.getText() + "','" +
-                                                            passwordInput.getText() + "')";
+        boolean checkField = isFieldEmpty();
 
-            stmt.executeUpdate(state);
+        if (!isDuplicate && !checkField) {
+            updateMainUserDatabase();
 
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Signup");
+            alert.setHeaderText("Signup completed");
+            alert.setContentText("Thank you for signup");
+            alert.showAndWait();
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            Stage stage = (Stage)registerPane.getScene().getWindow();
+            stage.close();
+            return;
         }
 
+        if (!checkField) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Signup");
+            alert.setHeaderText("Signup not completed");
+            alert.setContentText("Please check id for duplicate");
+            alert.showAndWait();
 
-        Stage stage = (Stage)registerPane.getScene().getWindow();
-        stage.close();
+        }
+
+    }
+
+    public boolean isFieldEmpty() {
+        boolean checkField = nameInput.getText().isEmpty() || usernameInput.getText().isEmpty() ||
+                passwordInput.getText().isEmpty();
+
+        if (checkField) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Signup");
+            alert.setHeaderText("Every field not completed");
+            alert.setContentText("Please fill every field to complete");
+            alert.showAndWait();
+        }
+
+        return checkField;
     }
 
     public void setCheckIdButton() {
+        try {
+            Connection con = h2Connector.getConnection();
+            Statement stmt = con.createStatement();
+            String state = "SELECT * FROM MAINUSER WHERE MAINUSERNAME = '" + usernameInput.getText() + "'";
+            ResultSet resultSet = stmt.executeQuery(state);
+            isDuplicate = resultSet.next();
+            disableCheckId();
 
+            con.close();
+            stmt.close();
+            resultSet.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateMainUserDatabase() {
+        try {
+            Connection con = h2Connector.getConnection();
+            Statement stmt = con.createStatement();
+            String state = "INSERT INTO MAINUSER VALUES('" + usernameInput.getText() + "','" + nameInput.getText()
+                            + "','" + passwordInput.getText() + "')";
+            stmt.executeUpdate(state);
+
+            con.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disableCheckId() {
+        usernameInput.setDisable(!isDuplicate);
+        if (!isDuplicate) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Signup");
+            alert.setHeaderText("Username available");
+            alert.setContentText("Username is available to use");
+            alert.showAndWait();
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Signup");
+        alert.setHeaderText("Username already exists!");
+        alert.setContentText("Please select another username");
+        alert.showAndWait();
     }
 }
