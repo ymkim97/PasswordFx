623d5415c1d17a34277058a754dd5f52822de7db
ymkim97 <rladudaud97@naver.com>
2023-01-13 19:58:44 +0900

    add encrypter decrypter class

---
+++ src/main/java/ymkim/passwordfx/Authenticator.java
@@ -0,0 +1,39 @@
+package ymkim.passwordfx;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class Authenticator {
+    private Cipher cipher;
+    private SecretKeySpec keySpec;
+    private IvParameterSpec ivParameterSpec;
+
+    public Authenticator() throws Exception{
+        String alg = "AES/CBC/PKCS5Padding";
+        String key = "73869011290132490853472803203527";
+        String iv = key.substring(0, 16);
+
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
+    }
+
+
+    public String encrypt(String text) throws Exception {
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
+
+        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public String decrypt(String cipherText) throws Exception {
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+        return new String(decrypted, "UTF-8");
+    }
+
+}
